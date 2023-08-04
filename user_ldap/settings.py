e8a29fef7b44c75d217025a0d27a75f2399d8992
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-08-04 09:24:30 -0500

    ultimas configuraciones

--- user_ldap/settings.py
+++ user_ldap/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import ldap
-from django_auth_ldap.config import LDAPSearch, LDAPGroupQuery,GroupOfNamesType,PosixGroupType
+from django_auth_ldap.config import LDAPSearch
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'django_auth_ldap'
 ]
 
 MIDDLEWARE = [
@@ -55,41 +56,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'user_ldap.urls'
 
 ##ldap
-AUTH_LDAP_SERVER_URI = 'ldap://localhost'
-AUTH_LDAP_BIND_DN = 'cn=admin,dc=example,dc=com'
-AUTH_LDAP_BIND_PASSWORD = 'YourLDAPPassword'
-AUTH_LDAP_USER_SEARCH = LDAPSearch('dc=example,dc=com',ldap.SCOPE_SUBTREE, '(uid=%(user)s)')
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch('dc=example,dc=com',ldap.SCOPE_SUBTREE, '(objectClass=top)')
-AUTH_LDAP_GROUP_TYPE = PosixGroupType(name_attr="cn")
-AUTH_LDAP_MIRROR_GROUPS = True
-
-    # Populate the Django user from the LDAP directory.
-AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=groups,dc=example,dc=com"
-
-AUTH_LDAP_USER_ATTR_MAP = {
-        "first_name": "givenName",
-        "last_name": "sn",
-        "email": "mail",
-        "username": "uid",
-        "password": "userPassword",
-}
-AUTH_LDAP_PROFILE_ATTR_MAP = {
-        "home_directory": "homeDirectory"
-}
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-        "is_active": "cn=active,ou=groups,dc=example,dc=com",
-        "is_staff": "cn=staff,ou=groups,dc=example,dc=com",
-        "is_superuser": "cn=superuser,ou=groups,dc=example,dc=com"
-}
-    
+AUTH_LDAP_SERVER_URI = "ldap://192.168.0.50"
+AUTH_LDAP_BIND_DN = "cn=admin2,dc=ejemplo,dc=local"
+AUTH_LDAP_BIND_PASSWORD = "Emiliano12"
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=Users,dc=ejemplo,dc=local", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
 AUTH_LDAP_ALWAYS_UPDATE_USER = True
-AUTH_LDAP_FIND_GROUP_PERMS = True
+
 AUTH_LDAP_CACHE_TIMEOUT = 3600
-    
-AUTH_LDAP_FIND_GROUP_PERMS = True
-    
-    # Keep ModelBackend around for per-user permissions and maybe a local
-    # superuser.
+
 AUTHENTICATION_BACKENDS = (
         'django_auth_ldap.backend.LDAPBackend',
         'django.contrib.auth.backends.ModelBackend',
